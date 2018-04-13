f0fcdf5f5329073752658ec7cdf6102e516d667f
Enejo James <spec.en.james@gmail.com>
2018-04-13 20:36:28 +0100

    Initial server setup

--- server/app.js
+++ server/app.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import logger from 'morgan';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = parseInt(process.env.PORT, 10) || 8000;
 
+app.use(bodyParser.json());
 app.use(logger('combined'));
 
-app.listen(port, () => {
-    console.log('We are live!');
+app.get('*', (req, res) => {
+    res.send('Right here!');
 });
+
+app.listen(port);
