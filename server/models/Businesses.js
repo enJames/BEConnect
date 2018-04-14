cc08ecece945993a06ae22efa49b5c1eac848318
Enejo James <spec.en.james@gmail.com>
2018-04-14 21:23:04 +0100

    Add dummy data models

---
+++ server/models/Businesses.js
@@ -0,0 +1,28 @@
+const Businesses = [
+    {
+        id: 1,
+        businessName: 'Mortiz Global',
+        category: 'Entertainment',
+        state: 'Lagos'
+    },
+    {
+        id: 2,
+        businessName: 'Suruzar Global',
+        category: 'Food',
+        state: 'Benue'
+    },
+    {
+        id: 3,
+        businessName: 'ES International',
+        category: 'Fashion',
+        state: 'Rivers'
+    },
+    {
+        id: 4,
+        businessName: 'Dreamzone Limited',
+        category: 'Hospitality',
+        state: 'Kano'
+    }
+];
+
+export default Businesses;
