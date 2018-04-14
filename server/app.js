a68a8a7294cbc2ff488d9fc1ff45f7a4d3099823
Enejo James <spec.en.james@gmail.com>
2018-04-14 21:21:10 +0100

    Remove catch all routes

--- server/app.js
+++ server/app.js
@@ -1,18 +1,17 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import Routes from './routes/Routes';
 
 const app = express();
+const urlencoded = bodyParser.urlencoded({ extended: false });
 const port = parseInt(process.env.PORT, 10) || 8000;
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // parses application/json
+app.use(urlencoded); // parses application/x-www.form-urlencoded
 app.use(logger('combined'));
 
-app.get('*', (req, res) => {
-    res.send({
-        message: 'Welcome to the start!'
-    });
-});
+Routes(app);
 
 app.listen(port);
 
