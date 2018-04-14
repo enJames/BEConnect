8920c30d2cbc5c53869c75a0918435bbe12b2e7d
Enejo James <spec.en.james@gmail.com>
2018-04-15 00:06:24 +0100

    Rewrite test.app.js

--- server/test/test.app.js
+++ server/test/test.app.js
@@ -6,40 +6,16 @@ chai.use(chaiHttp);
 const { assert, should } = chai;
 should();
 
-describe('SERVER TESTS', () => {
-    describe('When a user visits "/"', () => {
-        it('It should return a 200 status', (done) => {
-            chai.request(app)
-                .get('/')
-                .end((req, res) => {
-                    res.should.have.status(200);
-                    done();
-                });
-        });
-        it('It should return an object', (done) => {
-            chai.request(app)
-                .get('/')
-                .end((req, res) => {
-                    res.body.should.be.an('object');
-                    done();
-                });
-        });
-        it('It should a message "Welcome to the start!"', (done) => {
-            chai.request(app)
-                .get('/')
-                .end((req, res) => {
-                    assert.equal(res.body.message, 'Welcome to the start!', 'compare');
-                    done();
-                });
-        });
-    });
-});
-
 describe('POST BUSINESSES TESTS', () => {
     describe('When a user sends a POST request to "/businesses"', () => {
         it('It should return a 201 status', (done) => {
             chai.request(app)
-                .get('/')
+                .post('/businesses')
+                .send({
+                    businessName: 'Hamilton Berkleys',
+                    category: 'Food',
+                    state: 'Kano'
+                })
                 .end((req, res) => {
                     res.should.have.status(201);
                     done();
@@ -47,7 +23,12 @@ describe('POST BUSINESSES TESTS', () => {
         });
         it('It should return an object', (done) => {
             chai.request(app)
-                .get('/')
+                .post('/businesses')
+                .send({
+                    businessName: 'Hamilton Berkleys',
+                    category: 'Food',
+                    state: 'Kano'
+                })
                 .end((req, res) => {
                     res.body.should.be.an('object');
                     done();
@@ -55,9 +36,27 @@ describe('POST BUSINESSES TESTS', () => {
         });
         it('Response message should equal "Registration successful!"', (done) => {
             chai.request(app)
-                .get('/')
+                .post('/businesses')
+                .send({
+                    businessName: 'Hamilton Berkleys',
+                    category: 'Food',
+                    state: 'Kano'
+                })
                 .end((req, res) => {
                     assert.equal(res.body.message, 'Registration successful!', 'compare');
+                    done();
+                });
+        });
+        it('It should return a 400 if a field is not set."', (done) => {
+            chai.request(app)
+                .post('/businesses')
+                .send({
+                    businessName: 'Hamisc Berkleys',
+                    category: '',
+                    state: 'Sokoto'
+                })
+                .end((req, res) => {
+                    assert.equal(res.body.message, 'Fill out all fields', 'compare');
                     done();
                 });
         });
