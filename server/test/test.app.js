78b83cc659269144d19eba861b86d7a9ea2e6ab0
Enejo James <spec.en.james@gmail.com>
2018-04-14 23:27:37 +0100

    Write test for POST business

--- server/test/test.app.js
+++ server/test/test.app.js
@@ -6,7 +6,7 @@ chai.use(chaiHttp);
 const { assert, should } = chai;
 should();
 
-describe('Server test', () => {
+describe('SERVER TESTS', () => {
     describe('When a user visits "/"', () => {
         it('It should return a 200 status', (done) => {
             chai.request(app)
@@ -29,6 +29,35 @@ describe('Server test', () => {
                 .get('/')
                 .end((req, res) => {
                     assert.equal(res.body.message, 'Welcome to the start!', 'compare');
+                    done();
+                });
+        });
+    });
+});
+
+describe('POST BUSINESSES TESTS', () => {
+    describe('When a user sends a POST request to "/businesses"', () => {
+        it('It should return a 201 status', (done) => {
+            chai.request(app)
+                .get('/')
+                .end((req, res) => {
+                    res.should.have.status(201);
+                    done();
+                });
+        });
+        it('It should return an object', (done) => {
+            chai.request(app)
+                .get('/')
+                .end((req, res) => {
+                    res.body.should.be.an('object');
+                    done();
+                });
+        });
+        it('Response message should equal "Registration successful!"', (done) => {
+            chai.request(app)
+                .get('/')
+                .end((req, res) => {
+                    assert.equal(res.body.message, 'Registration successful!', 'compare');
                     done();
                 });
         });
