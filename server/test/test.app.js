8a787c97debe0a0823a3cc2ae60b7d1cb45fc3fc
Enejo James <spec.en.james@gmail.com>
2018-04-15 11:17:40 +0100

    Test update route

--- server/test/test.app.js
+++ server/test/test.app.js
@@ -7,10 +7,10 @@ const { assert, should } = chai;
 should();
 
 describe('POST BUSINESSES TESTS', () => {
-    describe('When a user sends a POST request to "/businesses"', () => {
+    describe('When a user sends a POST request to "/api/v1/businesses"', () => {
         it('It should return a 201 status', (done) => {
             chai.request(app)
-                .post('/businesses')
+                .post('/api/v1/businesses')
                 .send({
                     businessName: 'Hamilton Berkleys',
                     category: 'Food',
@@ -23,7 +23,7 @@ describe('POST BUSINESSES TESTS', () => {
         });
         it('It should return an object', (done) => {
             chai.request(app)
-                .post('/businesses')
+                .post('/api/v1/businesses')
                 .send({
                     businessName: 'Hamilton Berkleys',
                     category: 'Food',
@@ -36,7 +36,7 @@ describe('POST BUSINESSES TESTS', () => {
         });
         it('Response message should equal "Registration successful!"', (done) => {
             chai.request(app)
-                .post('/businesses')
+                .post('/api/v1/businesses')
                 .send({
                     businessName: 'Hamilton Berkleys',
                     category: 'Food',
@@ -49,7 +49,20 @@ describe('POST BUSINESSES TESTS', () => {
         });
         it('It should return a 400 if a field is not set."', (done) => {
             chai.request(app)
-                .post('/businesses')
+                .post('/api/v1/businesses')
+                .send({
+                    businessName: 'Hamisc Berkleys',
+                    category: '',
+                    state: 'Sokoto'
+                })
+                .end((req, res) => {
+                    res.should.have.status(400);
+                    done();
+                });
+        });
+        it('It should return a message "Fill out all fields" if a field (or more) is/are not set', (done) => {
+            chai.request(app)
+                .post('/api/v1/businesses')
                 .send({
                     businessName: 'Hamisc Berkleys',
                     category: '',
@@ -57,6 +70,66 @@ describe('POST BUSINESSES TESTS', () => {
                 })
                 .end((req, res) => {
                     assert.equal(res.body.message, 'Fill out all fields', 'compare');
+                    done();
+                });
+        });
+    });
+});
+
+describe('PUT BUSINESSES TESTS', () => {
+    describe('When a user sends a PUT request to "/api/v1/businesses/:businessId"', () => {
+        it('It should return a 200 status', (done) => {
+            chai.request(app)
+                .put('/api/v1/businesses/4')
+                .send({
+                    state: 'Oyo'
+                })
+                .end((req, res) => {
+                    res.should.have.status(200);
+                    done();
+                });
+        });
+        it('It should return an object', (done) => {
+            chai.request(app)
+                .put('/api/v1/businesses/3')
+                .send({
+                    category: 'Mining'
+                })
+                .end((req, res) => {
+                    res.body.should.be.an('object');
+                    done();
+                });
+        });
+        it('Response message should equal "Business updated!"', (done) => {
+            chai.request(app)
+                .put('/api/v1/businesses/4')
+                .send({
+                    state: 'Lagos'
+                })
+                .end((req, res) => {
+                    assert.equal(res.body.message, 'Business updated!', 'compare');
+                    done();
+                });
+        });
+        it('It should return a 404 status if business is not found."', (done) => {
+            chai.request(app)
+                .put('/api/v1/businesses/10')
+                .send({
+                    businessName: 'Hamisc Berkleys Limited'
+                })
+                .end((req, res) => {
+                    res.should.have.status(404);
+                    done();
+                });
+        });
+        it('It should return a message "Business not found." if business does not exist', (done) => {
+            chai.request(app)
+                .put('/api/v1/businesses/10')
+                .send({
+                    businessName: 'Hamisc Berkleys Limited'
+                })
+                .end((req, res) => {
+                    assert.equal(res.body.message, 'Business not found!', 'compare');
                     done();
                 });
         });
