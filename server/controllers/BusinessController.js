05ae264cbb9f1f4625cbbab14284bb106180bece
Enejo James <spec.en.james@gmail.com>
2018-04-15 09:05:38 +0100

    Add comments

--- server/controllers/BusinessController.js
+++ server/controllers/BusinessController.js
@@ -2,10 +2,14 @@ import Businesses from '../models/Businesses';
 import SendResponse from '../SendResponse';
 
 const BusinessController = {
+    // Register business
     create: (req, res) => {
+        // Assign user entries to variables
         const { businessName, category, state } = req.body;
 
+        // Validation of user input
         if (!businessName || !category || !state) {
+            // Send a validation failure response
             return SendResponse(res, 400, 'Fill out all fields');
         }
         const dataToPersist = {
@@ -15,8 +19,10 @@ const BusinessController = {
             state
         };
 
+        // Persist successful validation data to database
         Businesses.push(dataToPersist);
 
+        // Send a response after data persistence to database
         return SendResponse(res, 201, 'Registration successful!', Businesses);
     }
 };
