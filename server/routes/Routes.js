d54b7e9a598449906ba985da2963b29c10726a0c
Enejo James <spec.en.james@gmail.com>
2018-04-15 14:26:16 +0100

    Add getBusiness route

--- server/routes/Routes.js
+++ server/routes/Routes.js
@@ -4,6 +4,7 @@ const Routes = (app) => {
     app.post('/api/v1/businesses', BusinessController.create);
     app.put('/api/v1/businesses/:businessId', BusinessController.update);
     app.delete('/api/v1/businesses/:businessId', BusinessController.remove);
+    app.get('/api/v1/businesses/:businessId', BusinessController.getBusiness);
 };
 
 export default Routes;
